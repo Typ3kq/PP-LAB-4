6bf76c4c332198588ddeefcf6ae5ba1eb3174da9
Typ3kq <kacpertywonek60@gmail.comm>
2024-04-07 09:31:57 +0200

    zad3

--- Company.java
+++ Company.java
@@ -6,14 +6,21 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        // Dodawanie pracowników
         employees[0] = new Manager("Jan Kowalski", 20000, 0);
         employees[1] = new Worker("Mateusz Mateuszowski", 10000, "Kurier");
         employees[2] = new Employee("Grzegorz Grzegowski", 15000);
         employees[3] = new Worker("Andrzej Andrzejowski", 25000, "Piłkarz");
         employees[4] = new Worker("Agata Agatowska", 30000, "Barmanka");
 
-        // Wyświetlanie danych dla wszystkich pracowników
+        int nonManagerCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(7500);
+
         System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
