d76b03b1db741ee0aa685230e958d842e8d134cd
Typ3kq <kacpertywonek60@gmail.comm>
2024-04-07 09:56:04 +0200

    zad4

--- Company.java
+++ Company.java
@@ -4,24 +4,25 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Jan Kowalski", 20000, 0);
         employees[1] = new Worker("Mateusz Mateuszowski", 10000, "Kurier");
         employees[2] = new Employee("Grzegorz Grzegowski", 15000);
         employees[3] = new Worker("Andrzej Andrzejowski", 25000, "Piłkarz");
         employees[4] = new Worker("Agata Agatowska", 30000, "Barmanka");
+        employees[5] = new Manager("Jakub Jakubowski", 40000, 0);
+        employees[6] = new Worker("Stanisław Stanisłowski", 55000, "Tynkarz");
 
-        int nonManagerCount = 0;
         for (Employee employee : employees) {
-            if (!(employee instanceof Manager)) {
-                nonManagerCount++;
+            employee.setSalary(employee.getSalary() + 500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2); 
+                employee.setSalary(7500); 
             }
         }
-        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
-        employees[0].setSalary(7500);
 
-        System.out.println("Dane dla wszystkich pracowników:");
+        System.out.println("Zaktualizowane dane pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
